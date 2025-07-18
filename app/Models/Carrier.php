4b5294cf76c60e2306db25c791c23d4eef5c01e4
Wahyuadin <wahyuadinugraha2@gmail.com>
2025-07-18 17:23:14 +0700

    update

--- app/Models/Carrier.php
+++ app/Models/Carrier.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Carrier extends Model
 {
     use HasFactory;
+
+    public function departement()
+    {
+        return $this->belongsTo(DepartementCarrier::class);
+    }
+
+    public function location()
+    {
+        return $this->belongsTo(LocationCarrier::class);
+    }
+    public function pengalaman()
+    {
+        return $this->belongsTo(PengalamanCarrier::class);
+    }
+
+    public static function showData($id = null)
+    {
+        return $id ? self::find($id)->get() : self::where('is_active', true)->latest()->get();
+    }
 }
